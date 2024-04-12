a80a852dfd3ee01686b4ec38931da2edaeec2dcb
RedPanda0000 <154955239+RedPanda0000@users.noreply.github.com>
2024-04-13 02:20:33 +0900

    main on

---
+++ goodgame/goodgame.gg/src/main/java/fourjo/idle/goodgame/gg/repository/RankingRepository.java
@@ -0,0 +1,47 @@
+package fourjo.idle.goodgame.gg.repository;
+
+import fourjo.idle.goodgame.gg.web.dto.ranking.RankingDto;
+import fourjo.idle.goodgame.gg.web.dto.ranking.RankingSearchDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface RankingRepository {
+
+    //highrank_mst
+    public int insertRankingLeagueV4(Map<String,Object> insert);
+
+    public int updateRankingSummonerV4(Map<String,Object> update);
+
+    public int updateRankingAccountV1(Map<String,Object> update);
+
+    List<String> pullSummonerIdList();
+
+    List<String> pullPuuidList();
+
+    public int truncateTable();
+
+
+    public int insertLowRankingLeagueV4(Map<String,Object> insert);
+
+    public int updateLowRankingSummonerV4(Map<String,Object> update);
+
+    public int updateLowRankingAccountV1(Map<String,Object> update);
+
+    List<String> pullLowSummonerIdList();
+
+    List<String> pullLowPuuidList();
+
+    public int truncateLowTable();
+
+
+
+    public List<RankingDto> searchRankingList(RankingSearchDto rankingSearchDto);
+
+
+
+
+
+}
---
+++ goodgame/goodgame.gg/src/main/java/fourjo/idle/goodgame/gg/web/dto/ranking/RankingDto.java
@@ -0,0 +1,20 @@
+package fourjo.idle.goodgame.gg.web.dto.ranking;
+
+import lombok.Data;
+
+@Data
+public class RankingDto {
+    private int rankingIndex;
+    private String gameName;
+    private String tagLine;
+    private int summonerLevel;
+    private int profileIconId;
+    private String queueType;
+    private String tier;
+    private String rank;
+    private int leaguePoints;
+    private int wins;
+    private int losses;
+    private String winRate;
+}
+
